7626a47c957b6e4e6de0ec487fd3b4cbdb079e33
Artyom <cflabravo@yahoo.com>
2024-04-29 19:45:19 +0300

    Initial Commit

---
+++ bookblog/bookblogApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookblogApp.swift
+//  bookblog
+//
+//  Created by Artyom Danko on 29.04.24.
+//
+
+import SwiftUI
+
+@main
+struct bookblogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
